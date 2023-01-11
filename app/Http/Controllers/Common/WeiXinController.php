2d3c9575e56f532b99402399934dd6f876a04d72
chenzhongwei@kuaizi.ai <chenzhongwei@kuaizi.ai>
2023-01-11 14:54:41 +0800

    debug

--- app/Http/Controllers/Common/WeiXinController.php
+++ app/Http/Controllers/Common/WeiXinController.php
@@ -32,66 +32,6 @@ class WeiXinController extends BaseController
     }
 
     public function test(){
-
-
-
-        dump(strlen('亲爱的，
-
-我想说的是，我爱你。我爱你的一切，从你的笑容到你的眼睛，从你的声音到你的触感，从你的思想到你的行为，我都爱你。
-
-我爱你，因为你是我最好的朋友，你总是在我最需要的时候出现，你总是给我最好的建议，你总是支持我，你总是陪我一起度过最难熬的时刻。
-
-我爱你，因为你是我最亲密的伙伴，你总是陪我一起分享快乐，你总是陪我一起分担忧愁，你总是陪我一起度过最美好的时光。
-
-我爱你，因为你是我最坚强的支柱，你总是在我最艰难的时刻给我力量，你总是在我最迷茫的时刻给我方向，你总是在我最孤独的时刻给我温暖。
-
-我爱你，因为你是我最美丽的梦想，你总是在我最深的梦里出现，你总是在我最美的梦里出现，你总是在我最美好的梦里出现。
-
-我爱你，因为你是我最真实的感受，你总是在我最深的心里出现，你总是在我最真实的心里出现，你总是在我最真实的心里出现。
-
-我爱你，因为你是我最珍贵的礼物，你总是在我最珍贵的时刻出现，你总是在我最珍贵的地方出现，你总是在我最珍贵的心里出现。
-
-我爱你，因为你是我最美好的回忆，你总是在我最美好的时光里出现，你总是在我最美好的地方出现，你总是在我最美好的心里出现。
-
-我爱你，因为你是我最爱的人，你总是在我最爱的时刻出现，你总是在我最爱的地方出现，你总是在我最爱的心里出现。
-
-亲爱的，我爱你，永远爱你，直到永远。
-
-爱你的，
-XXX'));die;
-        /*dump((new WeiXinService())->custom_text('oERWv6qbxUaXC6Thly0ggeAkVilM','2c0geqIdBXLD1qjOERPKr9VEIOblffIi3EnLJC8bdn-wr3GMu02CXYd3yVK9WcBy','mpnews'));die;*/
-        /*$data = [
-            [
-                'title' => '临时文案',
-                'author' => 'czw',
-                'content' => '图文消息的具体内容，支持 HTML 标签，必须少于2万字符，小于1M，且此处会去除 JS ,涉及图片 url 必须来源 "上传图文消息内的图片获取URL"接口获取。外部图片 url 将被过滤。',
-                'thumb_media_id' => "2c0geqIdBXLD1qjOERPKr5TgdQbQTuIllAcyjdKddknAZ1YJocbNGlzFewYEwgcK",
-            ]
-        ];
-
-        dump((new WeiXinService())->draft_add($data));die;*/
-        /*$open = (new OpenApiService())->completions('说说对SQL语句优化有哪些方法？');
-        $choices = $open['choices'];
-        $text = '';
-        foreach ($choices as $value){
-            $text .= $value['text'];
-        }
-        dump($text);
-        $lines = explode("\n\n", $text);
-        array_shift($lines);
-        $text = implode("\n\n", $lines);
-        dump($text);die;*/
-        $msg = array (
-            'ToUserName' => 'gh_03aa44ccfbb4',
-            'FromUserName' => 'oERWv6qbxUaXC6Thly0ggeAkVilM',
-            'CreateTime' => '1655199131',
-            'MsgType' => 'text',
-            'Content' => '我是谁',
-        );
-        $user = (new MtUser())->getUserByWinXinId($msg['FromUserName']);
-        $CommonService = new CommonService();
-        dump($CommonService->manage($msg,$user));
-        die;
     }
 
     public function firstValid(Request $request){
